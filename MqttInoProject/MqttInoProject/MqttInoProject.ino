d07420e1a8631b07becfd09bca13d4e2c29b09f6
Raf Bronze <rnm1816@gmail.com>
2022-01-20 03:24:55 +0200

    reboot

--- MqttInoProject/MqttInoProject/MqttInoProject.ino
+++ MqttInoProject/MqttInoProject/MqttInoProject.ino
@@ -5,6 +5,9 @@ Hardware:
 -NodeMCU board
 -2 relays connected to Digital pins
 
+Virtual pins:
+V0: led
+
 This project controls 2 relays through the internet to implement a home automation.
 The duration of the ON relays is monitored and after 20min the ralays automatically turn OFF
 The date and the duration of the last ON is stored as history.
@@ -28,13 +31,19 @@ char MQTT_PORT[] = SECRET_MQTT_PORT;
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
+int led = 2;        //led pin
+int relay1 = 12;    //D6 pin
+int relay2 = 14;    //D5 pin
+int heater = 0;     //on-off
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  // Debug console
+  Serial.begin(9600);
   Cayenne.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, ssid, pass);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);
+  Cayenne.virtualWrite(V0, 0);
 }
 
 void loop() {
